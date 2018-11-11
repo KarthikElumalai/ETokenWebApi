6b612477194df8324927527dd924c10ed8e969a6
Karthik Elumalai <karthikstorms@gmail.com>
2018-11-11 20:06:11 +0530

    Add project files.

---
+++ ETokenApi/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETokenApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        // GET api/Login/5
+        [HttpGet("{username}")]
+        public ActionResult<string> Get(string  username)
+        {
+
+            if (username == "sachin")
+            {
+                return "Valid User";
+            }
+            return "Not Valid User";
+        }
+    }
+}
